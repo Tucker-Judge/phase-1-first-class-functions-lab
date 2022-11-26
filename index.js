67b46c25b296d32f6b5cae06c8c5bba2b95dd4ba
Tucker <tudge669@gmail.com>
2022-11-26 17:40:43 -0500

    commit tree 1d97974f64c962fce2844983e4b549d0d2cdfcb9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+let array = ['Antonio', 'Brick', 'Bro', 'Prince']
+let returnFirstTwoDrivers = (arg) => {
+    let newArray =[]
+  for (let i = 0; i<2; i++){
+      newArray.push(arg[i])
+}
+  return newArray
+    }
+
+let returnLastTwoDrivers = (arg) => {
+    let newArray = []
+    for(let i = arg.length - 1; i> arg.length - 3; i--){
+    newArray.unshift(arg[i])    
+}
+    return newArray
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(arg) {
+    function returned(fare) {
+        return fare * arg
+    }
+    return returned
+}
+
+const fareDoubler = fare => {
+return createFareMultiplier(2)(fare)
+}
+const fareTripler = fare => {
+    return createFareMultiplier(3)(fare)
+    }
+function selectDifferentDrivers(arg = [], arg2 = 'function'){
+    return arg2(arg)
+}
